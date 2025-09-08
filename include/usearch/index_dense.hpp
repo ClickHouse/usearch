f93a34236040a1891f14a80763aba4824a1ff8f1
Shankar Iyer <shankar.iyer@clickhouse.com>
2025-09-08 15:32:25 +0000

    Change alignment of graph allocator to 8

--- include/usearch/index_dense.hpp
+++ include/usearch/index_dense.hpp
@@ -403,7 +403,8 @@ class index_dense_gt {
     using serialization_config_t = index_dense_serialization_config_t;
 
     using dynamic_allocator_t = aligned_allocator_gt<byte_t, 64>;
-    using tape_allocator_t = memory_mapping_allocator_gt<64>;
+    /// ClickHouse-specific patch - change alignment to 8 (https://github.com/unum-cloud/usearch/issues/659)
+    using tape_allocator_t = memory_mapping_allocator_gt<8>;
 
   private:
     /// @brief Punned index.
